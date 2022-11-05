dc73d429db41bb0d92afe791548c2335b0d85673
Estebanfonseca <estebanp57@hotmail.com>
2022-11-04 22:27:59 -0500

    pagina de not found 404

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-
+import NotFoundPage from "./pages/NotFoundPage";
 import {Routes, Route} from 'react-router-dom'
 import { Home1 } from "./pages/Home1";
+import Home2 from "./pages/Home2";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Home1/>}/>
+        <Route path="/Notfound" element={<NotFoundPage/>}></Route>
+        <Route path="/home2" element={<Home2/>}></Route>
+        
       </Routes>
 
   );
