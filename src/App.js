f242b744e68fa441e9d93621a26d57a183538cce
Emilio Lubo <emilubo@msn.com>
2022-11-05 19:01:54 -0300

    MT-F1-07

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import NotFoundPage from "./pages/NotFoundPage";
 import {Routes, Route} from 'react-router-dom'
 import { Layout } from "./layout/Layout";
 import { Home } from "./pages/Home";
+import {SignUp} from './pages/SignUp.jsx'
 
 function App() {
   return (
         <Layout>
           <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/signup" element={<SignUp/>}/>
             <Route path="*" element={<NotFoundPage/>}></Route>
           </Routes>
         </Layout>
