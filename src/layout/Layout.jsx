720425bc28878eea0c34d337c23a84835133a065
Estebanfonseca <estebanp57@hotmail.com>
2022-11-05 13:44:39 -0500

    pagina singin

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { NavBar } from '../components/NavBar'
 import Footer from '../components/Footer'
 import ScrollToTop from '../components/ScrollToTop'
+import Login from '../pages/SingIn'
 
 export const Layout = ({children}) => {
   return (
     <div>
         <NavBar/>
+        <Login/>
         {children}
         <ScrollToTop/>
         <Footer/>
