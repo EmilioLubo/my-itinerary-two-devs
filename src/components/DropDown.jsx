959a3f5e6456f167f95efe63d9c55e92adb17bd1
Emilio Lubo <emilubo@msn.com>
2022-11-07 00:55:25 -0300

    fix MT-F1-03

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,9 +1,19 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 
 export const DropDown = () => {
 
     let [isOpen, setIsOpen] = useState(false)
+
+    useEffect(() => {
+        if(isOpen){
+            document.addEventListener('mousedown', e => {
+                if(e.target.className !== 'nav-link' && e.target.className !== 'drop-icon'){
+                    setIsOpen(!isOpen)
+                }
+            })
+        }
+    })
 
     let keyHandler = (ev) => {
         if(ev.key === 'Escape'){
@@ -19,10 +29,10 @@ export const DropDown = () => {
                 <button onClick={dropHandler}>
                     <img className='drop-icon' src={isOpen ? '/img/dropdown_up_icon.png' : '/img/dropdown_icon.png'} alt='dropdown icon' />
                 </button>
-                <ul className={`${isOpen ? '' : 'hidden'} drop-list`}>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to="/cities">Cities</Link></li>
-                    <li><Link to='/Hotels'>Hotels</Link></li>
+                <ul className={`${isOpen ? '' : 'hidden'} drop-list flex f-column g-1 p-1 list-border`}>
+                    <li><Link className='nav-link' to='/'>Home</Link></li>
+                    <li><Link className='nav-link' to="/cities">Cities</Link></li>
+                    <li><Link className='nav-link'to='/Hotels'>Hotels</Link></li>
                 </ul>
             </div>
   )
