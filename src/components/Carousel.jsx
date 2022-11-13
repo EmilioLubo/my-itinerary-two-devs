14f130a95e345768396d2dd4a91be715753c786f
Estebanfonseca <estebanp57@hotmail.com>
2022-11-12 19:04:49 -0500

    cambio total de fetch a axios

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Arrow from "./Arrow";
 import Photo from "./Photo";
+import axios from "axios";
+import apiUrl from "../url";
 
 export default function Carousel() {
     let [num, setNum] = useState(0);
@@ -12,35 +14,35 @@ export default function Carousel() {
     let [data3, setData3] = useState([]);
 
     const dato = async (num) => {
-        let res = await fetch("/data/cities.json");
-        res = await res.json();
-        res = res.cities;
+        let res = await axios.get(`${apiUrl}/cities`);
+        res = res.data.response;
         let dato = [...res.slice(0, 6)];
         res = res.slice(0, 7);
         res = res[num];
         setData(res);
         setId(dato);
     };
+
+
+    
     const dato2 = async (num) => {
-        let res = await fetch("/data/cities.json");
-        res = await res.json();
-        res = res.cities;
+        let res = await axios.get(`${apiUrl}/cities`);
+        res = res.data.response;
         res = res.slice(6, 13);
         res = res[num];
         setData2(res);
     };
     const dato1 = async (num) => {
-        let res = await fetch("/data/hotels.json");
-        res = await res.json();
-        res = res.hotels;
+        let res = await axios.get(`${apiUrl}/hotels`);
+        res = res.data.response;
+        console.log(res)
         res = res.slice(0, 6);
         res = res[num];
         setData1(res);
     };
     const dato3 = async (num) => {
-        let res = await fetch("/data/hotels.json");
-        res = await res.json();
-        res = res.hotels;
+        let res = await axios.get(`${apiUrl}/hotels`);
+        res = res.data.response;
         res = res.slice(6, 13);
         res = res[num];
         setData3(res);
