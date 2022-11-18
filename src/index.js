8cce815ba6405f3c149c1a24fab9ff3e3c7f5422
Estebanfonseca <estebanp57@hotmail.com>
2022-11-18 18:26:30 -0500

    redux

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import {BrowserRouter} from 'react-router-dom'
 import {AutoToTop} from './components/AutoToTop'
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  
     <BrowserRouter>
       <AutoToTop>
-        <App/>
+        <Provider store={store}>
+          <App/>
+        </Provider>
       </AutoToTop>
     </BrowserRouter>
+  
 );
