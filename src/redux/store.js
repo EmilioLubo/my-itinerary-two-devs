751d20e0ccf77bd028faa88795172a85dda11e78
Estebanfonseca <estebanp57@hotmail.com>
2022-11-19 15:14:56 -0500

    add redux fo render cards

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import hotelReducer from "./reducers/hotelReducer";
 
 export const store = configureStore({
-    reducer:{}
+    reducer:{
+        hotelReducer
+    }
 })
